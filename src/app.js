4e83adbbaf0d8fc7b0bdf97aa37e0f08f4516a50
Julio Scanoni <juliosc4noni@gmail.com>
2024-10-03 16:13:19 -0300

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 
 class App{
@@ -17,6 +19,13 @@ class App{
     }
 
     middlewares(){
+        this.server.use(cors());
+
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'uploads'))
+        );
+
         this.server.use(express.json());
     }
 
